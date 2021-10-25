8b4a303600d96bdd875d9c7c185cca70471cf6ae
dev-2046 <xifeng.jin@harrys.com>
2021-10-25 14:11:45 -0400

    add user validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,14 @@
-class ArticlesController < ApplicationController
+# frozen_string_literal: true
 
-  before_action :set_article, only: [:show, :edit, :destroy, :update]
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit destroy update]
   def index
     @articles = Article.all
   end
 
-  def edit
-  end
+  def edit; end
 
-  def show
-  end
+  def show; end
 
   def new
     @article = Article.new
@@ -18,17 +17,16 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:notice] = 'Article was successfully created'
       redirect_to @article
     else
       render 'new'
     end
-      
   end
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:notice] = 'Article was successfully updated'
       redirect_to @article
     else
       render 'edit'
@@ -41,10 +39,11 @@ class ArticlesController < ApplicationController
   end
 
   private
+
   def set_article
     @article = Article.find(params[:id])
   end
-  
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
