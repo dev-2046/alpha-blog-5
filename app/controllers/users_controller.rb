17491f9aa982596340e6bb294992b9deb5f33464
dev-2046 <xifeng.jin@harrys.com>
2021-10-25 21:41:12 -0400

    add update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   
   def new
     @user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account information has been updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -15,6 +28,9 @@ class UsersController < ApplicationController
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
