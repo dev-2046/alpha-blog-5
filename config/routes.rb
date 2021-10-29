269cb3237c181eab6132e36ac5356f9122bbaeda
dev-2046 <xifeng.jin@harrys.com>
2021-10-28 21:03:02 -0400

    test driven development for categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :users, expect: [:new]
+  resources :users, except: [:new]
+  resources :categories, except: [:destroy]
 end
