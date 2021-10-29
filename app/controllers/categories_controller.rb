059b70d5d26e215ddab5254b72154f036f77746d
dev-2046 <xifeng.jin@harrys.com>
2021-10-28 21:25:09 -0400

    add category controller test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,32 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
   def new
+    @category = Category.new
   end
 
   def index
   end
 
   def show
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Your category has been created"
+      redirect_to @category      
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
